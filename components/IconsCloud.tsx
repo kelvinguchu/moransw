bf15fc61334c92fba63e631bd8819ef4025cf6cb
kelvinguchu <kelvinguchu5@gmail.com>
2024-09-11 20:22:02 +0300

    mobile icons update

--- components/IconsCloud.tsx
+++ components/IconsCloud.tsx
@@ -1,5 +1,4 @@
 import IconCloud from "@/components/ui/icon-cloud";
-import useWindowSize from "../hooks/useWindowSize";
 
 const slugs = [
   "typescript",
@@ -40,15 +39,11 @@ const slugs = [
 ];
 
 export function IconsCloud() {
-  const { width } = useWindowSize();
-  const isMobile = width ? width < 768 : false; // Adjust this breakpoint as needed, handle undefined case
-
-  const adjustedSlugs = isMobile ? slugs.slice(0, 15) : slugs; // Reduce icons for mobile
 
   return (
     <section className='relative w-full h-[300px] md:h-[100%] flex justify-center items-center' >
       <div className='relative flex h-full w-full  items-center justify-center overflow-hidden rounded-lg'>
-        <IconCloud iconSlugs={adjustedSlugs} />
+        <IconCloud iconSlugs={slugs} />
       </div>
     </section>
   );
